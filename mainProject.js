99eb4305eed67a32e2aaedd6f802b9c45b795a91
iesusdavila <irdavila@espol.edu.ec>
2022-03-27 16:30:51 -0500

    Volvi a valer paloma por crear una ruta que era la carpeta del repositorio anterior

--- mainProject.js
+++ mainProject.js
@@ -1,53 +1,51 @@
 function selectionEquip1() {
   if (document.getElementById("genero").value == "eqMen") {
-    document.getElementById("team").src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/icons/jugadores.png";
+    document.getElementById("team").src = "icons/jugadores.png";
 
     document.getElementsByClassName("img-equipment")[0].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/men/first_equipment_front.jpg";
+      "assets/men/first_equipment_front.jpg";
 
     document.getElementsByClassName("img-equipment")[1].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/men/first_equipment_back.jpg";
+      "assets/men/first_equipment_back.jpg";
   } else {
-    document.getElementById("team").src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/icons/jugadoras.jpg";
+    document.getElementById("team").src = "icons/jugadoras.jpg";
 
     document.getElementsByClassName("img-equipment")[0].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/woman/first_equipment_front.jpg";
+      "assets/woman/first_equipment_front.jpg";
 
     document.getElementsByClassName("img-equipment")[1].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/woman/first_equipment_back.jpg";
+      "assets/woman/first_equipment_back.jpg";
   }
 }
 
 function selectionEquip2() {
   if (document.getElementById("genero").value == "eqMen") {
     document.getElementsByClassName("img-equipment")[0].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/men/second_equipment_front.jpg";
+      "assets/men/second_equipment_front.jpg";
 
     document.getElementsByClassName("img-equipment")[1].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/men/second_equipment_back.jpg";
+      "assets/men/second_equipment_back.jpg";
   } else {
     document.getElementsByClassName("img-equipment")[0].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/woman/second_equipment_front.jpg";
+      "assets/woman/second_equipment_front.jpg";
 
     document.getElementsByClassName("img-equipment")[1].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/woman/second_equipment_back.jpg";
+      "assets/woman/second_equipment_back.jpg";
   }
 }
 
 function selectionEquip3() {
   if (document.getElementById("genero").value == "eqMen") {
     document.getElementsByClassName("img-equipment")[0].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/men/third_equipment_front.jpg";
+      "assets/men/third_equipment_front.jpg";
 
     document.getElementsByClassName("img-equipment")[1].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/men/third_equipment_back.jpg";
+      "assets/men/third_equipment_back.jpg";
   } else {
     document.getElementsByClassName("img-equipment")[0].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/woman/third_equipment_front.jpg";
+      "assets/woman/third_equipment_front.jpg";
 
     document.getElementsByClassName("img-equipment")[1].src =
-      "file:///C:/Users/USUARIO/Desktop/Platzi/Curso%20Practico%20de%20JavaScript/Curso-basico-JavaScript/assets/woman/third_equipment_back.jpg";
+      "assets/woman/third_equipment_back.jpg";
   }
 }
